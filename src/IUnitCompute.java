cd1840a60d4da575454faa0c453ce9c795aee558
amritas02 <bees_inthegarden@yahoo.co.in>
2017-04-27 18:33:40 +0530

    Update IUnitCompute.java

--- src/IUnitCompute.java
+++ src/IUnitCompute.java
@@ -6,8 +6,9 @@ this interface is implemented by UnitCompute
 todo - more operations needs to be added
 */
 public interface IUnitCompute extends Remote{
-  public DoubleMatrix add(DoubleMatrix A, DoubleMatrix B)throws RemoteException;
-  public DoubleMatrix sub(DoubleMatrix A, DoubleMatrix B)throws RemoteException;
-  public DoubleMatrix mul(DoubleMatrix A, DoubleMatrix B)throws RemoteException;
+  public double add(double A, double B)throws RemoteException;
+  public double sub(double A, double B)throws RemoteException;
+  public double mul(double A, double B)throws RemoteException;
+  public DoubleMatrix mmul(DoubleMatrix A, DoubleMatrix B) throws RemoteException; 
 
 }
